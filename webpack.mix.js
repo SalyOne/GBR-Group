ddfaef623045f78bfd267c9b9014f4cf626141d5
SalyOne <salome.batkhadze@gmail.com>
2022-03-04 03:49:18 +0400

    initial commit

---
+++ webpack.mix.js
@@ -0,0 +1,8 @@
+
+let mix = require('laravel-mix');
+// require('laravel-mix-serve');
+
+// mix.serve('php artisan serve --port=8888');
+
+mix.js('src/js/app.js', 'public/assets/dist')
+.sass('src/scss/app.scss', 'public/assets/dist')
