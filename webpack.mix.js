ed950aa956dbd1684963ac9f3cd8773eafbb4b87
SalyOne <salome.batkhadze@gmail.com>
2022-05-11 20:55:03 +0400

    fix font and structure problem

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,8 +1,9 @@
-
+const path = require('path');
 let mix = require('laravel-mix');
 // require('laravel-mix-serve');
 
 // mix.serve('php artisan serve --port=8888');
 
-mix.js('src/js/app.js', 'public/assets/dist')
-.sass('src/scss/app.scss', 'public/assets/dist')
+mix.js('src/js/app.js', 'assets/dist')
+.sass('src/scss/app.scss', 'assets/dist')
+.setPublicPath('public');
