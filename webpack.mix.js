a947763954be3479196a454b0967dcaf7d0c60bb
SalyOne <salome.batkhadze@gmail.com>
2022-06-01 19:03:05 +0400

    add change import to use

--- webpack.mix.js
+++ webpack.mix.js
@@ -7,3 +7,22 @@ let mix = require('laravel-mix');
 mix.js('src/js/app.js', 'assets/dist')
 .sass('src/scss/app.scss', 'assets/dist')
 .setPublicPath('public');
+
+mix.extend('fixSassResourcesLoader', (webpackConfig) => {
+    const scssRule = webpackConfig.module.rules.find((rule) => {
+        const { test } = rule;
+        return test && typeof test.test === 'function' &&  test.test('.scss');
+    });
+
+    if (!scssRule) return;
+
+    scssRule.oneOf.forEach((oneOf) => {
+        oneOf.use.forEach(({ loader, options}) => {
+            if (loader === 'sass-resources-loader') {
+                options.hoistUseStatements = true;
+            }
+        });
+    })
+});
+
+mix.fixSassResourcesLoader();
